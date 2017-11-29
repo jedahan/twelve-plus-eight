e636b7d5ad4afba8474b8039a7815def8b5b4580
Jonathan Dahan <hi@jonathan.is>
2017-11-29 11:57:25 +0100

    we have square

--- src/twelve_plus_eight/core.cljs
+++ src/twelve_plus_eight/core.cljs
@@ -27,29 +27,41 @@
    :angle (+ (:angle state) 0.01)})
 
 (defn sign [x]
-  (if
+  (cond
     (> x 0) 1
-    (if
-      (< x 0) -1
-      0
+    (< x 0) -1
+    :else 0
+  )
+)
+
+(defn circle [radius theta]
+  [
+    (* (q/abs (q/sin theta)) radius (sign (q/cos theta)))
+    (* (q/abs (q/sin theta)) radius (sign (q/sin theta)))
+  ]
+)
+
+(def half-pi (/ js/Math.PI 2))
+(def quarter-pi (/ js/Math.PI 4))
+
+(defn square [theta]
+  (let [quadrant (js/Math.floor (mod (/ (+ theta quarter-pi) half-pi) 4))]
+    (cond
+        (= quadrant 0) [(+ 1) (* (q/tan theta) 1)]
+        (= quadrant 1) [(/ 1 (q/tan theta)) (+ 1)]
+        (= quadrant 2) [(- 1) (- (* (q/tan theta) 1))]
+        (= quadrant 3) [(- (/ 1 (q/tan theta))) (- 1)]
     )
   )
 )
 
-(defn squircle-x [r t]
-  (* (q/sqrt (q/abs (q/cos t)))
-     r
-     (sign (q/cos t))
-  )
-)
+;(defn squarcle-x [r t squareness]
+;  (q/lerp (circle r t) (square r t) squareness)
+;)
 
-(defn squircle-y [r t]
-  (* (q/sqrt (q/abs (q/sin t)))
-     r
-     (sign (q/sin t))
-  )
-)
-
+;(defn squarcle-y [r t squareness]
+;  (q/lerp (circle-y r t) (square-y r t) squareness)
+;)
 
 (defn draw-state [state]
   (let [[mx my] (mouse-normalized)]
@@ -62,13 +74,20 @@
     (q/fill (:color state) 255 255)
     ; Calculate x and y coordinates of the circle.
     (let [angle (:angle state)
-          x (squircle-x (* mx 360) angle)
-          y (squircle-y (* my 360) angle)]
+         coords  (square angle)
+         x (first coords)
+         y (second coords)]
       ; Move origin point to the center of the sketch.
       (q/with-translation [(/ (q/width) 2)
                            (/ (q/height) 2)]
         ; Draw the circle.
-        (q/ellipse x y 10 10)))
+        (q/ellipse (* x 50) (* y 50) 10 10))
+
+      (q/fill 100 100 255)
+      (q/rect 0 (- (q/height) 40) (q/width) 40)
+      (q/fill 0 0 100)
+      (q/text (str (q/floor x) " " (q/floor y)) 10 (- (q/height) 10))
+    )
   )
 )
 
